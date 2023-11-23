ef0db6f8105b4401031481dfcc1b08950a5173bd
Antoine Petraki <apetrakifr@hotmail.com>
2023-11-22 23:10:43 -0500

    First Commit

---
+++ Extra_Credit_Arduino_Code.ino
@@ -0,0 +1,82 @@
+/*
+  Arduino Starter Kit example
+  Project 6 - Light Theremin
+
+  This sketch is written to accompany Project 6 in the Arduino Starter Kit
+
+  Parts required:
+  - photoresistor
+  - 10 kilohm resistor
+  - piezo
+
+  created 13 Sep 2012
+  by Scott Fitzgerald
+
+  https://store.arduino.cc/genuino-starter-kit
+
+  This example code is part of the public domain.
+*/
+
+#include <Arduino.h>
+#include <SD.h>  // Include the SD library
+
+// variable to hold sensor value
+int sensorValue;
+// variable to calibrate low value
+int sensorLow = 1023;
+// variable to calibrate high value
+int sensorHigh = 0;
+// LED pin
+const int ledPin = 13;
+
+// File for writing analog values
+File dataFile;
+
+void setup() {
+  // Initialize serial communication
+  Serial.begin(9600);
+
+  
+  // Make the LED pin an output and turn it on
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  // Open the file for writing
+  dataFile = SD.open("analog_values.txt", FILE_WRITE);
+  
+  // calibrate for the first five seconds after program runs
+  while (millis() < 5000) {
+    // record the maximum sensor value
+    sensorValue = analogRead(A0);
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+    }
+    // record the minimum sensor value
+    if (sensorValue < sensorLow) {
+      sensorLow = sensorValue;
+    }
+  }
+  // turn the LED off, signaling the end of the calibration period
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+  // read the input from A0 and store it in a variable
+  sensorValue = analogRead(A1);
+
+  // map the sensor values to a wide range of pitches
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+
+  // print the analog input value on a new line
+  Serial.println(sensorValue);
+  //Serial.print('3');
+
+  // write the analog input value to the file
+  dataFile.println(sensorValue);
+
+  // play the tone for 20 ms on pin 8
+  tone(8, pitch, 20);
+
+  // wait for a moment
+  delay(10);
+}
